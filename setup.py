a734eab2b4381d9cf36be0a0ee10eeb480327fd5
Dor Hay <dhay@attentigroup.com>
2020-11-01 15:32:03 +0200

    Update Package Name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="dorhay",
+    name="geodist",
     version="0.1",
     author="Dor Hay",
     author_email="dorhay@gmail.com",
